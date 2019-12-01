48267f70564042bde66031b5a9b0ae999f252c30
Suprianto Djamalu <supriantodjamalu@Supriantos-MacBook-Pro.local>
2019-12-01 13:22:04 +0800

    nominal tidak sama

--- pro_input_transaksi.php
+++ pro_input_transaksi.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 
 include "lib/config.php";
 $tgl            =    $_POST['tgl'];
@@ -7,16 +8,42 @@ $ref    =    $_POST['ref'];
 $ket            =     $_POST['ket'];
 $nominal    =     $_POST['nominal'];
 $tipe            =    $_POST['tipe'];
-mysqli_query($config->koneksi(), "INSERT INTO  `sia`.`tb_jurnal` (
-`id` ,
-`tgl` ,
-`id_akun` ,
-`ket` ,
-`ref` ,
-`nominal` ,
-`tipe`
-)
-VALUES (
-NULL ,  '$tgl',  '$id_akun',  '$ket',  '$ref',  '$nominal',  '$tipe'
-);");
-header('location: jurnalumum.php');
+
+if ($tipe == "K") {
+    $cek = mysqli_fetch_assoc(mysqli_query($config->koneksi(), "SELECT SUM(nominal) as nominal FROM tb_jurnal WHERE tipe='D' AND tgl='$tgl'"))['nominal'];
+
+    if ($cek != $nominal) {
+        echo "<script>
+        window.alert('Nominal tidak sama dengan Debet.');
+        window.location.href='".$siteurl."/jurnalumum.php';
+        </script>";
+    } else {
+        mysqli_query($config->koneksi(), "INSERT INTO  `sia`.`tb_jurnal` (
+            `id` ,
+            `tgl` ,
+            `id_akun` ,
+            `ket` ,
+            `ref` ,
+            `nominal` ,
+            `tipe`
+            )
+            VALUES (
+            NULL ,  '$tgl',  '$id_akun',  '$ket',  '$ref',  '$nominal',  '$tipe'
+            );");
+        header('location: jurnalumum.php');
+    }
+} else {
+    mysqli_query($config->koneksi(), "INSERT INTO  `sia`.`tb_jurnal` (
+        `id` ,
+        `tgl` ,
+        `id_akun` ,
+        `ket` ,
+        `ref` ,
+        `nominal` ,
+        `tipe`
+        )
+        VALUES (
+        NULL ,  '$tgl',  '$id_akun',  '$ket',  '$ref',  '$nominal',  '$tipe'
+        );");
+    header('location: jurnalumum.php');
+}
